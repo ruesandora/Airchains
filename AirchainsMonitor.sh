f36bc54f10a86cde1b7d25eed7378ee0ec8f74ac
Ahmet Kaan <63106683+Dwtexe@users.noreply.github.com>
2024-07-02 16:46:17 +0300

    Update AirchainsMonitor.sh

--- AirchainsMonitor.sh
+++ AirchainsMonitor.sh
@@ -162,9 +162,15 @@ process_log_line() {
 		echo
 	fi
 	
+    local modified=$(echo "$line" | sed -E 's/^(.*Error=").*account sequence mismatch, expected ([0-9]+), got ([0-9]+): incorrect account sequence.*$/\1account sequence mismatch, expected \2, got \3: incorrect account sequence"/')
     
-    echo "$line"
-
+    if [[ "$line" != "$modified" ]]; then
+        echo "$modified"
+    else
+		echo "$line"
+	fi
+	
+	
     LAST_5_LINES+=("$line")
 
     if [ ${#LAST_5_LINES[@]} -gt 5 ]; then
@@ -175,13 +181,14 @@ process_log_line() {
         handle_error "Failed to get transaction by hash: not found (occurred twice in last 5 lines)"
     elif [ $(echo "${LAST_5_LINES[@]}" | grep -o "error code: '13' msg: 'insufficient fees" | wc -l) -ge 2 ]; then
         handle_error "error code: '13' msg: 'insufficient fees (occurred twice in last 5 lines)"
+	elif [ $(echo "${LAST_5_LINES[@]}" | grep -o "failed to execute message; message index: 0" | wc -l) -ge 2 ]; then
+        handle_error "failed to execute message; message index: 0 (occurred twice in last 5 lines)"
     elif echo "$line" | grep -q -F "Failed to Validate VRF" ||
          echo "$line" | grep -q -F "Failed to Init VRF" ||
          echo "$line" | grep -q -F "Failed to Transact Verify pod" ||
          echo "$line" | grep -q -F "Client connection error: error while requesting node" ||
          echo "$line" | grep -q -F "Switchyard client connection error" ||
          echo "$line" | grep -q -F "error in json rpc client, with http response metadata:" ||
-         echo "$line" | grep -q -F "failed to execute message; message index: 0" ||
          echo "$line" | grep -q -F "rpc error: code = Unknown desc = rpc error: code = Unknown desc = failed to execute message"; then
         handle_error "$line"
     fi
