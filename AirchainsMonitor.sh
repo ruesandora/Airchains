59f0a28371cde22f13d36435af61f39a82186b61
Ahmet Kaan <63106683+Dwtexe@users.noreply.github.com>
2024-07-02 20:43:29 +0300

    Update AirchainsMonitor.sh

--- AirchainsMonitor.sh
+++ AirchainsMonitor.sh
@@ -136,12 +136,6 @@ handle_error() {
         cecho  "$RED" "Failed to rollback. Exiting..."
         cecho  "$RED" "Run this script on the tracks/ folder."
         exit 1
-    fi
-    if ! go run cmd/main.go rollback; then
-        cecho  "$RED" "Failed to rollback. Exiting..."
-        cecho  "$RED" "Run this script on the tracks/ folder."
-        exit 1
-    fi
     cecho "$GREEN" "=> Successfully ran rollback commands"
     cecho "$YELLOW" "Restarting stationd service..."
     sudo systemctl restart stationd > /dev/null 2>&1
