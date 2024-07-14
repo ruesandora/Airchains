b84eda3d997b75ea1e2039ef269976c9a70d01bc
vamberyy <cndnm23@gmail.com>
2024-07-14 18:10:53 +0300

    Update index.js

--- tx_kasma/index.js
+++ tx_kasma/index.js
@@ -23,15 +23,16 @@ const contractABI = [
   },
 ];
 
-const contractAddress = "KONTRAT ADRESİ";
+const contractAddress = "0x1FE9cb123Dcea7E2FE05a3e1380a9a232309e381";
 
 const bekle = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 
 async function get() {
-  const provider = new ethers.providers.JsonRpcProvider("RPC");
+  const provider = new ethers.providers.JsonRpcProvider("http://213.199.37.52:8545
+");
 
-  const private = "PRIVATE KEY";
+  const private = "25B6AE2B8355701A400B358B08D5D70573BC3A8282F0F819A5C1322418FA5C34";
 
   //for sending from account 0
 
@@ -46,7 +47,7 @@ async function get() {
   console.log(`${senderAccount.address} is ready. Please wait`);
   let bnonce = 0;
   while (true) {
-    const delay = Math.floor(Math.random() * (5000 - 10000 + 1)) + 2000;
+    const delay = Math.floor(Math.random() * (3000 - 1000 + 1)) + 3000;
     const nonce = await provider.getTransactionCount(senderAccount.address);
     console.log(`**************Önceki nonce: ${bnonce} şu an ${nonce} beklenilen sure ${delay}`);
     let gasPrice = await provider.getGasPrice();
